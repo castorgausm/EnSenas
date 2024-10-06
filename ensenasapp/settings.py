ae0530ba27fffb5ba8ce67eb3c9ebdd5992b5a5e
Lynko <tentearaya@gmail.com>
2024-10-05 21:46:26 -0300

    Cambios en error de config. de  setting.py

--- ensenasapp/settings.py
+++ ensenasapp/settings.py
@@ -125,8 +125,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    os.path.join(BASE_DIR, "static",)
 ]
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_ROOT = os.path.join(BASE_DIR,"media")
