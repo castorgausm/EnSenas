d34652d72471e36155f06cdeb6863ece028cfe48
castorgausm <castorga@usm.cl>
2024-10-12 20:44:05 -0300

    Corrección urls

--- ensenasapp/urls.py
+++ ensenasapp/urls.py
@@ -20,8 +20,12 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 from django.urls import path
-from .views import ensenas_view
+from . import views
 
 urlpatterns = [
-    path('', ensenas_view, name='home'),
+    path('', views.start_view, name='start'),
+    path('register/', views.register_view, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('home/', views.home_view, name='home'),
+    path('lesson/', views.lesson_view, name='lesson'),
 ]
--- ensenasapp/views.py
+++ ensenasapp/views.py
@@ -1,5 +1,17 @@
 
 from django.shortcuts import render
 
-def ensenas_view(request):
+def start_view(request):
     return render(request, 'start.html')
+
+def register_view(request):
+    return render(request, 'register.html')
+
+def login_view(request):
+    return render(request, 'login.html')
+
+def home_view(request):
+    return render(request, 'home.html')
+
+def lesson_view(request):
+    return render(request, 'lesson.html')
