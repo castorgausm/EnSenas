73a46b3799d09e10fad9fc7dddb902ab0c9c3825
castorgausm <castorga@usm.cl>
2024-10-15 18:04:44 -0300

    Función de registro e inicio de sesión

--- ensenasapp/views.py
+++ ensenasapp/views.py
@@ -1,16 +1,60 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
 def start_view(request):
     return render(request, 'start.html')
-
-def register_view(request):
-    return render(request, 'register.html')
-
-def login_view(request):
-    return render(request, 'login.html')
 
 def home_view(request):
     return render(request, 'home.html')
 
 def lesson_view(request):
     return render(request, 'lesson.html')
+
+def register_view(request):
+    if request.method == 'POST':
+        firstname = request.POST['first_name']
+        lastname = request.POST['last_name', '']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        confirmpassword = request.POST['confirm_password']
+    
+        if password == confirmpassword:
+            if User.objects.filter(email=email).exists():
+                messages.error(request, 'El correo ya está registrado.')
+            elif User.objects.filter(username=username).exists():
+                messages.error(request, 'El nombre de usuario ya está registrado.')
+            else:
+                user = User.objects.create_user(
+                    first_name=firstname,
+                    last_name=lastname,
+                    username=username,
+                    email=email,
+                    password=password
+                )
+                user.save()
+
+                user = authenticate(request, username=username, password=password)
+                
+                if user is not None:
+                    login(request, user)
+                    messages.success(request, 'Registrado con éxito.')
+                    return redirect('home')
+        else:
+            messages.error(request, 'Las contraseñas no coinciden.')
+    return render(request, 'register.html')
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Datos incorrectos.')
+    return render(request, 'login.html')
