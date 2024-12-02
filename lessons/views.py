7847a33565e9f5d99566f6814d31b7f3efea331e
benjamin armijo <benjamin3armijo@gmail.com>
2024-12-02 16:15:16 -0300

    Añadir logica lecciones, añadir "leccion 4", y dis

--- lessons/views.py
+++ lessons/views.py
@@ -2,6 +2,12 @@ from django.http import Http404
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from lessons.models import Lesson, LessonImage,LessonUser
+from django.http import HttpResponse
+import json
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+
+
 
 @login_required
 def detail(request, number):
@@ -25,6 +31,66 @@ def list(request):
     
     return render(request, "lessons.html", {"lessons": lessons})
 
+@csrf_exempt
 @login_required
 def responder_leccion(request,number):
-    pass
+
+    user = request.user
+    try:
+        lesson = LessonUser.objects.get(lesson__number=number,user=user)
+    except Lesson.DoesNotExist:
+        raise Http404("Lesson does not exist")
+    
+    data = json.loads(request.body)
+    respuesta = data.get('respuesta')
+    # respuesta = request.POST.get('respuesta')
+    is_correct = respuesta.lower().strip() == lesson.lesson.response.lower().strip()
+
+    if is_correct:
+        lesson.finished = True
+        lesson.save()
+
+
+    return JsonResponse({
+            'correct': is_correct,
+    })
+
+@csrf_exempt
+@login_required
+def terminar_leccion(request,number):
+
+    user = request.user
+    try:
+        lesson = LessonUser.objects.get(lesson__number=number,user=user)
+    except Lesson.DoesNotExist:
+        raise Http404("Lesson does not exist")
+    
+    lesson.finished = True
+    lesson.save()
+    return HttpResponse("terminado", content_type='plain/txt')
+    
+    
+    # if respuesta.lower() == lesson.lesson.response:
+    #     lesson.finished = True
+    #     lesson.save()
+    #     print("Correcta")
+    #     return HttpResponse("correct", content_type='plain/txt')
+    # else: 
+    #     print("Incorrecta")
+    #     return HttpResponse("incorrect", content_type='plain/txt')
+
+
+
+    
+
+
+
+
+
+
+    
+    
+
+
+
+
